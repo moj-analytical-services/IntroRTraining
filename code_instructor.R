b805744e4913b0ecd95edd6cd08f8babb6d6dddd
Aidan Mews <aidan.mews@justice.gsi.gov.uk>
2018-07-27 16:00:59 +0000

    Corrected inconsistencies between the scripts and the markdown files

--- code_instructor.R
+++ code_instructor.R
@@ -1,16 +1,18 @@
 # 1 Introduction to R -----------------------------------------------------
 
-# 1.1 Command Console
+# 1.5 Command Console
 
 x <- 3
 
 x
 
-# 1.3 Other windows and getting help
+x <- c(3, 2, 4)
+
+# 1.6 Other windows and getting help
 
 ?mean
 
-# 1.4 exercises
+# 1.7 exercises
 # Q1 Create a new source code file in which you can store all commands you make during this exercise. Save it as ‘Intro_R_Exercises.R’.
 
 # Q2 Create a new value called y which is equal to 17.
@@ -27,13 +29,13 @@ y * 78
 
 # 2.1 Setting up a working directory
 
-# set working directory (the following path only works on DOM1)
-# setwd("S:/HQ/102PF/Shared/CJG_OMS/OMS/Analytical Services/RACC/Statistical Project Delivery/3. Training and development/R training/Introduction")
-
 # get working directory
 getwd()
 
-#2.2 Packages
+# set working directory 
+setwd("/home")
+
+# 2.2 Packages
 
 # install.packages("tidyverse")
 
@@ -61,54 +63,21 @@ str(offenders)
 
 summary(offenders)
 
-head(offenders,10)
+offenders[500,4]
 
-tail(offenders, 2)
+offenders[c(500, 502),4]
 
-offenders[c(500, 502),4:5]
+offenders[500,1:5]
 
 # 2.5 Dataset variables
 
 offenders$GENDER
 
-offenders[,4]
-
-select(offenders, LAST, FIRST)
-
 offenders$weight_kg <- offenders$WEIGHT*0.454
-
-?mutate
-
-offenders <- mutate(offenders, weight_kg = WEIGHT * 0.454)
 
 View(offenders)
 
-offenders <- rename(offenders, DoB = BIRTH_DATE)
-
-
-
-# 2 Exercises
-# Q1 What are the mean, max and min for variable AGE in the dataset offenders?
-# can find these values individually:
-mean(offenders$AGE)
-max(offenders$AGE)
-min(offenders$AGE)
-
-# or use summary:
-summary(offenders$AGE)
-
-# Q2 Use mutate to create a new variable providing the number of (full) years over 16 and to drop the original WEIGHT variable.
-
-# first drop the weight column
-offenders_new <- select(offenders, -WEIGHT)
-
-# then create years over 16 variable
-offenders_new <- mutate(offenders_new, years_over_16 = AGE - 16)
-
-
-# Calculations ------------------------------------------------------------
-
-# 3.1 Data classes
+# 2.6 Data classes
 
 class(offenders$WEIGHT)
 
@@ -128,11 +97,35 @@ offenders$tall <- offenders$HEIGHT > 175
 
 class(offenders$tall)
 
-# 3.2 Ifelse
+# 2.7 Ifelse
 
 offenders$wt_under_90  <- ifelse(offenders$weight_kg<90, 1, 0)
 
-# 3.3 Grouping and summarising data
+# 2 Exercises
+# Q1 What are the mean, max and min for variable AGE in the dataset offenders?
+# can find these values individually:
+mean(offenders$AGE)
+max(offenders$AGE)
+min(offenders$AGE)
+
+# or use summary:
+summary(offenders$AGE)
+
+# Q2 Use mutate to create a new variable providing the number of (full) years over 16 and to drop the original WEIGHT variable.
+
+# first drop the weight column
+offenders_new <- select(offenders, -WEIGHT)
+
+# then create years over 16 variable
+offenders_new <- mutate(offenders_new, years_over_16 = AGE - 16)
+
+# Q3 Create a new variable called ‘height_under_150’ which is 1 if under 150 cm and 0 otherwise.
+offenders$height_under_150 <- ifelse(offenders$HEIGHT < 150, 1, 0)
+
+
+# 3.	Data wrangling and ‘group by’ calculations --------------------------------------
+
+# 3.1 Grouping and summarising data
 
 ?dplyr::summarise
 ?group_by
@@ -145,55 +138,83 @@ regional_gender_average <- offenders %>% group_by(REGION, GENDER) %>%
 
 regional_gender_average <- ungroup(regional_gender_average)
 
-# 3.4 Filter
+# 3.2 Filter
 
 offenders %>% group_by(SENTENCE) %>% summarise(Count = n())
 
 crt_order_average <- offenders %>% filter(SENTENCE == "Court_order" & AGE > 50) %>% group_by(REGION, GENDER) %>%
   summarise(Ave = mean(PREV_CONVICTIONS))
 
+# 3.3 Select
+
+offenders_anonymous <- select(offenders, -LAST, -FIRST, -BLOCK)
+
+offenders_anonymous <- select(offenders, BIRTH_DATE, WEIGHT, PREV_CONVICTIONS)
+
+# 3.4 Rename
+
+offenders_anonymous <- offenders %>%
+  select(BIRTH_DATE, WEIGHT, PREV_CONVICTIONS) %>%
+  rename(DoB = BIRTH_DATE) 
+
+offenders_anonymous <- offenders %>%
+  select(BIRTH_DATE, WEIGHT, PREV_CONVICTIONS) %>%
+  rename(DoB = BIRTH_DATE, Num_prev_convictions = PREV_CONVICTIONS) 
+
+# 3.5 Mutate
+
+?mutate
+
+offenders_anonymous <- offenders %>%
+  select(BIRTH_DATE, WEIGHT, PREV_CONVICTIONS) %>%
+  rename(DoB = BIRTH_DATE, Num_prev_convictions = PREV_CONVICTIONS) %>%
+  mutate(weight_kg = WEIGHT * 0.454)
+
 # 3.6 Exercises -----------------------------------------------------------
 # Q1 Using group_by and summarise, calculate the average and median age for females in the West.
 offenders %>% group_by(GENDER, REGION) %>%
 summarise(mean(AGE), median(AGE))
 
-# Q2 Create a new variable called ‘height_under_150’ which is 1 if under 150 cm and 0 otherwise.
-offenders$height_under_150 <- ifelse(offenders$HEIGHT < 150, 1, 0)
-
-# Q3 How many have heights of less than 4 feet, what are their (recorded) heights and gender(s)?
+# Q2 How many have heights of less than 4 feet, what are their (recorded) heights and gender(s)?
 filter(offenders, HEIGHT < 400)
 
-# Q4 Produce a table showing the counts of height including missing values.
+# Q3 Produce a table showing the counts of height (including missing values).
 counts_of_height <- offenders %>% group_by(HEIGHT) %>%
   summarise(Count=n())
 
+# Q4	Create a new dataset containing PREV_CONVICTIONS and SENTENCE variables, rename 
+# SENTENCE as sentence_type, and create a new variable num_convictions that is 
+# PREV_CONVICTIONS + 1 (to take account of the latest conviction).
+offenders_new <- offenders %>%
+  select(SENTENCE, PREV_CONVICTIONS) %>%
+  rename(sentence_type = SENTENCE) %>%
+  mutate(num_convictions = PREV_CONVICTIONS + 1)
 
 # Dates -------------------------------------------------------------------
-# 4.1 
+# 4.1 Manipulating dates 
+
 library(lubridate)
 
+today()
 
-Sys.Date()
+offenders<- mutate(offenders, DoB_formatted = mdy(BIRTH_DATE))
 
-offenders$DoB_formatted <-  as.Date(offenders$DoB, "%m/%d/%Y")
+class(offenders$DoB_formatted)
 
+offenders <- mutate(offenders, day = day(DoB_formatted))
 
-offenders$b_wkday <- weekdays(offenders$DoB_formatted)
+offenders <- mutate(offenders, quarter = quarter(DoB_formatted))
 
-offenders$b_qtr <- quarters(offenders$DoB_formatted)
+offenders <- mutate(offenders, year = year(DoB_formatted))
 
+offenders <- mutate(offenders, month = month(DoB_formatted))
 
+offenders <- mutate(offenders, weekday = weekdays(DoB_formatted))
 
-offenders$b_year <- year(offenders$DoB_formatted)
-
-offenders$b_month <- month(offenders$DoB_formatted)
-
-offenders$b_day <- day(offenders$DoB_formatted)
-
-offenders$days_before_2000 <- as.Date("2000-01-01") - offenders$DoB_formatted
+offenders <- mutate(offenders, days_before_2000 = ymd("2000-01-01") - DoB_formatted)
 
 # 4.2 Exercises
-# Q1 Read in dataset 'FTSE_12_14.csv' and convert the variable date to class date.
+# Q1 Read in dataset ‘FTSE_12_14.csv’ and convert the variable date to class date. 
 
 # analytical platform amazon server:
 ftse <- s3tools::s3_path_to_full_df("alpha-everyone/R_training_intro/FTSE_12_14.csv")
@@ -212,16 +233,16 @@ ftse <- ftse %>%
 class(ftse$formatted_date)
 
 # Q2 Add a variable called day with the day of the week, and another variable called 
-# daily_performance for how much the share price has increased or decreased that day
+# daily_performance for how much the share price has increased or decreased that day 
 # (close price - open price). 
 
 # create weekday variable
-ftse <- mutate(ftse, day = day(converted_date))
+ftse <- mutate(ftse, weekday = weekdays(formatted_date))
 
 # add daily performance column
 ftse <- mutate(ftse, daily_performance = Close - Open)
 
-# Q3 Work out which day of the week has the highest mean performance using summarise(). 
+# Q3 Work out which day of the week has the highest mean performance. 
 
 weekday_performance <- ftse %>%
   group_by(weekday) %>%
@@ -240,12 +261,12 @@ offenders_trial <- read_csv("Offenders_Chicago_Police_Dept_Trial.csv")
 # read in data on analytical platform amazon server:
 offenders_trial  <- s3tools::s3_path_to_full_df("alpha-everyone/R_training_intro/Offenders_Chicago_Police_Dept_Trial.csv")
 
-offenders_merge <- dplyr::inner_join(offenders, offenders_trial, by=c("LAST", "DoB")) 
+offenders_trial <- dplyr::rename(offenders_trial, BIRTH_DATE=DoB) 
+
+offenders_merge <- dplyr::inner_join(offenders, offenders_trial, by=c("LAST", "BIRTH_DATE")) 
 
 men <- filter(offenders, GENDER == "MALE") 
-
 women <- filter(offenders, GENDER == "FEMALE")
-
 rejoined <- bind_rows(men, women)
 
 nrow(rejoined) # 1413 rows
@@ -262,7 +283,7 @@ complete.cases(offenders)
 
 complete_offenders <- filter(offenders, complete.cases(offenders))
 
-# 5.3
+# 5.3 Exporting data
 write.csv(complete_offenders, file = "Complete_offenders.csv")
 
 # 5.4 Exercises 
--- code_participant.R
+++ code_participant.R
@@ -1,12 +1,14 @@
-# 2.1
+# 2. Processing Data ------------------------------------------------------
 
-# set working directory (the following path only works on DOM1)
-# setwd("S:/HQ/102PF/Shared/CJG_OMS/OMS/Analytical Services/RACC/Statistical Project Delivery/3. Training and development/R training/Introduction")
+# 2.1 Setting up a working directory
 
 # get working directory
 getwd()
 
-#2.2
+# set working directory 
+setwd("/home")
+
+# 2.2 Packages
 
 # Install packages
 install.packages("tidyverse")
@@ -16,7 +18,7 @@ library("tidyverse")
 
 help(package=dplyr)
 
-# 2.3
+# 2.3 Importing data
 
 # If the csv file is in your working directory
 offenders <- read_csv("Offenders_Chicago_Police_Dept_Main.csv")
@@ -24,39 +26,29 @@ offenders <- read_csv("Offenders_Chicago_Police_Dept_Main.csv")
 # From the Analytical Platform amazon server
 offenders <- s3tools::s3_path_to_full_df("alpha-everyone/R_training_intro/Offenders_Chicago_Police_Dept_Main.csv")
 
-# 2.4
+# 2.4 Inspecting the dataset
 
-View(offenders)
+View(offenders) # note the capital V!
 
 str(offenders)
 
 summary(offenders)
 
-head(offenders,10)
+offenders[500,4]
 
-tail(offenders, 2)
+offenders[c(500, 502),4]
 
-offenders[c(500, 502),4:5]
+offenders[500,1:5]
 
-# 2.5
+# 2.5 Dataset variables
 
 offenders$GENDER
 
-offenders[,4]
-
-select(offenders, LAST, FIRST)
-
 offenders$weight_kg <- offenders$WEIGHT*0.454
-
-?mutate
-
-offenders <- mutate(offenders, weight_kg = WEIGHT * 0.454)
 
 View(offenders)
 
-offenders <- rename(offenders, DoB = BIRTH_DATE)
-
-# 3.1
+# 2.6 Data classes
 
 class(offenders$WEIGHT)
 
@@ -76,11 +68,13 @@ offenders$tall <- offenders$HEIGHT > 175
 
 class(offenders$tall)
 
-# 3.2
+# 2.7 Ifelse
 
 offenders$wt_under_90  <- ifelse(offenders$weight_kg<90, 1, 0)
 
-# 3.3
+# 3.	Data wrangling and ‘group by’ calculations --------------------------------------
+
+# 3.1 Grouping and summarising data
 
 ?dplyr::summarise
 ?group_by
@@ -93,12 +87,37 @@ regional_gender_average <- offenders %>% group_by(REGION, GENDER) %>%
 
 regional_gender_average <- ungroup(regional_gender_average)
 
-# 3.4
+# 3.2 Filter
 
 offenders %>% group_by(SENTENCE) %>% summarise(Count = n())
 
 crt_order_average <- offenders %>% filter(SENTENCE == "Court_order" & AGE > 50) %>% group_by(REGION, GENDER) %>%
   summarise(Ave = mean(PREV_CONVICTIONS))
+
+# 3.3 Select
+
+offenders_anonymous <- select(offenders, -LAST, -FIRST, -BLOCK)
+
+offenders_anonymous <- select(offenders, BIRTH_DATE, WEIGHT, PREV_CONVICTIONS)
+
+# 3.4 Rename
+
+offenders_anonymous <- offenders %>%
+  select(BIRTH_DATE, WEIGHT, PREV_CONVICTIONS) %>%
+  rename(DoB = BIRTH_DATE) 
+
+offenders_anonymous <- offenders %>%
+  select(BIRTH_DATE, WEIGHT, PREV_CONVICTIONS) %>%
+  rename(DoB = BIRTH_DATE, Num_prev_convictions = PREV_CONVICTIONS) 
+
+# 3.5 Mutate
+
+?mutate
+
+offenders_anonymous <- offenders %>%
+  select(BIRTH_DATE, WEIGHT, PREV_CONVICTIONS) %>%
+  rename(DoB = BIRTH_DATE, Num_prev_convictions = PREV_CONVICTIONS) %>%
+  mutate(weight_kg = WEIGHT * 0.454)
 
 # Dates -------------------------------------------------------------------
 # 4.1 Manipulating dates
@@ -107,21 +126,22 @@ library(lubridate)
 
 today()
 
-
-offenders<- mutate(offenders, DoB_formatted = mdy(DoB))
+offenders<- mutate(offenders, DoB_formatted = mdy(BIRTH_DATE))
 
 class(offenders$DoB_formatted)
 
-
 offenders <- mutate(offenders, day = day(DoB_formatted))
-      
+
 offenders <- mutate(offenders, quarter = quarter(DoB_formatted))
-      
+
 offenders <- mutate(offenders, year = year(DoB_formatted))
-      
+
 offenders <- mutate(offenders, month = month(DoB_formatted))
 
+offenders <- mutate(offenders, weekday = weekdays(DoB_formatted))
+
 offenders <- mutate(offenders, days_before_2000 = ymd("2000-01-01") - DoB_formatted)
+
 
 # 4.2 Exercises
 
@@ -131,21 +151,28 @@ ftse <- s3tools::s3_path_to_full_df("alpha-everyone/R_training_intro/FTSE_12_14.
 
 # 5.1
 
+# 5 Merging and exporting data --------------------------------------------
+# 5.1 Merging datasets
+
 # read in data on DOM1 (assuming file in your working directory):
 offenders_trial <- read_csv("Offenders_Chicago_Police_Dept_Trial.csv")
 
 # read in data on analytical platform amazon server:
 offenders_trial  <- s3tools::s3_path_to_full_df("alpha-everyone/R_training_intro/Offenders_Chicago_Police_Dept_Trial.csv")
 
-offenders_merge <- dplyr::inner_join(offenders, offenders_trial, by=c("LAST", "DoB")) 
+offenders_trial <- dplyr::rename(offenders_trial, BIRTH_DATE=DoB) 
 
-men <- filter(offenders, GENDER == "MALE")
+offenders_merge <- dplyr::inner_join(offenders, offenders_trial, by=c("LAST", "BIRTH_DATE")) 
 
+men <- filter(offenders, GENDER == "MALE") 
 women <- filter(offenders, GENDER == "FEMALE")
-
 rejoined <- bind_rows(men, women)
 
-# 5.2
+nrow(rejoined) # 1413 rows
+
+nrow(offenders) # 1413 rows
+
+# 5.2 Handling missing values
 
 height_table <- offenders %>% group_by(HEIGHT) %>% summarise(Count=n())
 
@@ -155,7 +182,6 @@ complete.cases(offenders)
 
 complete_offenders <- filter(offenders, complete.cases(offenders))
 
-# 5.3
-
+# 5.3 Exporting data
 write.csv(complete_offenders, file = "Complete_offenders.csv")
 
